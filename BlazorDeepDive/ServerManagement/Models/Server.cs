cb291e0e5349ca90cfaf9452f33625e1f729c67e
alexkolochev <alexkolochev@mail.ru>
2024-06-20 14:43:00 +0300

    Add project files.

---
+++ BlazorDeepDive/ServerManagement/Models/Server.cs
@@ -0,0 +1,17 @@
+﻿namespace ServerManagement.Models
+{
+    public class Server
+    {
+        public int ServerId { get; set; }
+        public bool IsOnline { get; set; }
+        public string? Name { get; set; }
+        public string? City { get; set; }
+
+        public Server()
+        {
+            Random random = new();
+            int randomNmber = random.Next(0 ,2);
+            IsOnline = randomNmber != 0;
+        }
+    }
+}
